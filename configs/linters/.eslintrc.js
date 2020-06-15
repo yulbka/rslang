02ed81b5b23ac8e74da8e795fbd544a222d9a27e
marianna <vergunmari@gmail.com>
2020-06-15 22:53:30 +0300

    feat: add webpack config for HTML/JS/PostCSS, linters on git hooks and minimal files for start

---
+++ configs/linters/.eslintrc.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const root = path.resolve(__dirname, '../..');
+
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: ['import', 'prettier'],
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:import/errors', 'plugin:import/warnings', 'prettier'],
+  rules: {
+    'no-use-before-define': 1,
+    'no-restricted-globals': 0,
+    'no-plusplus': 0,
+    'no-unused-expressions': 1,
+    'no-restricted-syntax': 0,
+    'no-throw-literal': 0,
+    'no-underscore-dangle': 0,
+    'no-async-promise-executor': 0,
+    'no-new': 0,
+    camelcase: 0,
+    'consistent-return': 1,
+    radix: ['error', 'as-needed'],
+    'spaced-comment': 0,
+    'new-cap': 1,
+    'import/no-extraneous-dependencies': 0, //because of Yarn Workspaces
+    'import/prefer-default-export': 0,
+    'import/no-cycle': 0,
+    'import/newline-after-import': 0
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [`${root}/src`],
+        extensions: ['.js', '.json'],
+      },
+    },
+  },
+};
---
+++ configs/linters/.stylelintrc.js
@@ -0,0 +1,47 @@
+const path = require('path'),
+  root = path.resolve(__dirname, '../../');
+
+module.exports = {
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-airbnb',
+    'stylelint-config-standard',
+    'stylelint-config-prettier',
+  ],
+  plugins: [
+    'stylelint-scss',
+    'stylelint-order',
+    'stylelint-declaration-block-no-ignored-properties',
+    'stylelint-high-performance-animation',
+    'stylelint-no-unsupported-browser-features',
+    'stylelint-declaration-use-variable'
+  ],
+  rules: {
+    'plugin/declaration-block-no-ignored-properties': true,
+    'plugin/no-low-performance-animation-properties': [
+      true,
+      { ignoreProperties: ['color', 'background-color', 'border-color'] },
+    ],
+    'plugin/no-unsupported-browser-features': [
+      true,
+      {
+        browsers: require(`${root}/package.json`).browserslist,
+        severity: 'warning',
+      },
+    ],
+    /*'sh-waqar/declaration-use-variable': [['/color/', /!*'font-size',*!/ /!*"z-index"*!/
+      { ignoreValues: ['inherit', 'transparent', 'currentColor'] },
+    ]],*/
+    'order/order': ['custom-properties', 'dollar-variables', 'declarations', 'at-rules', 'rules'],
+    'order/properties-alphabetical-order': true,
+    'selector-type-no-unknown': [true, { ignore: ['custom-elements'] }],
+    'block-no-empty': null,
+    'at-rule-no-unknown': null,
+    'max-nesting-depth': null,
+    'no-descending-specificity': null,
+    'no-missing-end-of-source-newline': null,
+    'comment-empty-line-before': null,
+    'comment-whitespace-inside': null,
+    'selector-max-id': null,
+  },
+};
