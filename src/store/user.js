6abe6f9164340a89390145d3645dc842c177ebbe
marianna <vergunmari@gmail.com>
2020-06-19 00:02:43 +0300

    feat: restructure user store

--- src/store/user.js
+++ src/store/user.js
@@ -2,37 +2,25 @@
     "email": "string3242424@dfgdg.dg",
     "password": "ASD324sdfs3@$@",
     "userId": "5eea492edffad00017faa81c",
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZWE0OTJlZGZmYWQwMDAxN2ZhYTgxYyIsImlhdCI6MTU5MjQxMjc1NiwiZXhwIjoxNTkyNDI3MTU2fQ.Pl3XwXnqs9MdMOuvTIS2GKGChJcmFf548QJyzwLkzss",
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZWE0OTJlZGZmYWQwMDAxN2ZhYTgxYyIsImlhdCI6MTU5MjUxMjU4NSwiZXhwIjoxNTkyNTI2OTg1fQ.CzGcz5F8sMod2gEQhn4RfIym574z0DZyLRfflqKpyeA",
 }*/
 
 export const userSettings = {
-  email: null,
-  password: null,
-  token: null,
-  userId: null,
-  wordsPerDay: null,
-  // ...userExample,
-  words: {
-    amount: null,
-    default: 50,
-    get amountOfNewWords() {
-      return this.amount ?? this.default;
-    },
+  auth: {
+    email: null,
+    password: null,
+    token: null,
+    userId: null,
   },
-  cards: {
-    maxAmount: null,
-    default: 50,
-    get amountOfNewCards() {
-      return this.maxAmount ?? this.default;
-    },
-    informationOfCard: {
-      translation: true,
-      sentenceWithExplanation: false,
-      sentenceWithExample: false,
-      transcription: true,
-      image: true,
-    },
+  learning: {
+    wordsPerDay: 50,
+    newWordsPerDay: 50, //<= wordsPerDay
+    withTranslation: true,
+    withExplanation: false,
+    withExample: false,
+    withTranscription: true,
+    withHelpImage: true,
     showAnswerButton: false,
   },
-  errors: {},
+  // ...userExample,
 };
