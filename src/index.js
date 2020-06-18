0d8dc7305b14d100855a6f72d80dcb99e25926ba
marianna <vergunmari@gmail.com>
2020-06-19 00:04:15 +0300

    feat: add examples for work with User API

--- src/index.js
+++ src/index.js
@@ -4,8 +4,36 @@ import './css/index.scss';
 import { store } from './store';
 import { API_USER } from './api/user';
 
+//example
 (async () => {
-  const res = await API_USER.getUser({ userId: '5eea492edffad00017faa81c' });
-  store.user = { ...store.user, ...res };
-  console.log({ ...store });
+  const [user, userSettings] = await Promise.all([
+    API_USER.getUser({ userId: '5eea492edffad00017faa81c' }),
+    API_USER.getUserSettings({
+      userId: '5eea492edffad00017faa81c',
+    }),
+  ]);
+
+  Object.entries(user).forEach(([key, value]) => {
+    store.user.auth[key] = value;
+  });
+
+  Object.entries(userSettings).forEach(([key, value]) => {
+    store.user.learning[key] = value;
+  });
+
+  /*API_USER.setUserSettings({
+        userId: '5eea492edffad00017faa81c',
+        userSettings: {
+            "wordsPerDay": 551,  // must be less than or equal to 1000
+            "optional": {
+                "translation": true,
+                "withExplanation": false,
+                "withExample": false,
+                "transcription": true,
+                "image": false
+            }
+        }
+    });*/
+
+  console.log('usr', { ...store });
 })();
