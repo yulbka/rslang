62b430158b382b82cf4df8d1ed739354a9be6023
marianna <vergunmari@gmail.com>
2020-06-19 23:20:05 +0300

    feat: add synchronization user settings with store

--- src/index.js
+++ src/index.js
@@ -1,39 +1,32 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './css/index.scss';
-import { store } from './store';
 import { API_USER } from './api/user';
+import { createSettingsBlock } from './pages/main';
+import { store } from './store';
+
+window.onload = async () => {
+  await initRequests();
+
+  createSettingsBlock();
+};
+
+async function initRequests() {
+  //await auth()
+  const userSettings = await API_USER.getUserSettings({
+    userId: store.user.auth.userId,
+  });
+
+  store.user.learning = {
+    ...store.user.learning,
+    ...userSettings,
+  };
+}
 
 //example
-(async () => {
-  const [user, userSettings] = await Promise.all([
-    API_USER.getUser({ userId: '5eea492edffad00017faa81c' }),
-    API_USER.getUserSettings({
-      userId: '5eea492edffad00017faa81c',
-    }),
-  ]);
-
-  Object.entries(user).forEach(([key, value]) => {
-    store.user.auth[key] = value;
-  });
-
-  Object.entries(userSettings).forEach(([key, value]) => {
-    store.user.learning[key] = value;
-  });
-
-  /*API_USER.setUserSettings({
-        userId: '5eea492edffad00017faa81c',
-        userSettings: {
-            "wordsPerDay": 551,  // must be less than or equal to 1000
-            "optional": {
-                "translation": true,
-                "withExplanation": false,
-                "withExample": false,
-                "transcription": true,
-                "image": false
-            }
-        }
-    });*/
-
-  console.log('usr', { ...store });
-})();
+// (async () => {
+//   const user = await API_USER.getUser({ userId: '5eea492edffad00017faa81c' })
+//   Object.entries(user).forEach(([key, value]) => {
+//     store.user.auth[key] = value;
+//   });
+// })();
