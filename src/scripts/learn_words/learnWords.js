0f1ca2f327744e4d44baa14f7c381cd19d61d9dc
Yuliya Ramanouskaya <yulbka13@mail.ru>
2020-06-22 20:28:28 +0300

    feat: play audio after word entry

--- src/scripts/learn_words/learnWords.js
+++ src/scripts/learn_words/learnWords.js
@@ -51,23 +51,26 @@ export class LearnWords {
 
   static showAnswerHandler() {
     const learnPage = document.querySelector('.learn-wrapper');
-    learnPage.addEventListener('click', (event) => {
+    learnPage.addEventListener('click', async (event) => {
       const target = event.target.closest('.show-answer');
       if (!target) return;
       this.showAnswer();
+      await this.playAudio();
       this.goToNextCard();
     });
   }
 
-  static checkAnswer() {
+  static async checkAnswer() {
     const activeSlide = document.querySelector('.swiper-slide-active');
     const input = activeSlide.querySelector('.card-input');
     const letters = activeSlide.querySelectorAll('.letter-hidden');
     if (input.value.toLowerCase() === input.dataset.word.toLowerCase()) {
       this.showAnswer();
+      await this.playAudio();
       this.goToNextCard();
     } else {
       this.hightLightAnswer(input, letters);
+      this.playAudio();
       input.addEventListener('input', () => {
         letters.forEach((letter) => {
           letter.classList.remove('text-success', 'text-warning', 'text-danger', 'letter-transparent');
@@ -75,7 +78,7 @@ export class LearnWords {
         });
       });
     }
-    input.value = '';
+    input.value = ''; 
   }
 
   static hightLightAnswer(input, letters) {
@@ -112,5 +115,41 @@ export class LearnWords {
     mySwiper.allowSlideNext = true;
     mySwiper.slideNext();
     mySwiper.allowSlideNext = false;
+  }
+
+  static async playAudio() {
+    const playList = [];
+    const activeSlide = document.querySelector('.swiper-slide-active');
+    const audioWord = activeSlide.querySelector('.audio-word');
+    playList.push(audioWord);
+    const audioExample = activeSlide.querySelector('.audio-example');
+    if (audioExample) {
+      playList.push(audioExample);
+    }
+    const audioMeaning = activeSlide.querySelector('.audio-meaning');
+    if (audioMeaning) {
+      playList.push(audioMeaning);
+    }
+    return new Promise((resolve) => {
+      playList.forEach((sound, index) => {
+        const audio = sound;
+        const input = activeSlide.querySelector('.card-input');
+        input.addEventListener('input', () => {
+          audio.pause();
+          audio.currentTime = 0;
+        });  
+        if (index === 0) {
+          audio.play();
+        } else {
+          playList[index - 1].addEventListener('ended', () => {
+            audio.play();
+          });
+        }
+        playList[playList.length - 1].addEventListener('ended', () => {
+          resolve();
+        }); 
+      }); 
+    })
+    
   }
 }
