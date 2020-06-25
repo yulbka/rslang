e58fb0f31516ce63e20e25d9c213a2d33ef9db41
marianna <vergunmari@gmail.com>
2020-06-26 01:32:41 +0300

    refactor: update routesMap, create children maps

--- src/scripts/helpers/variables.js
+++ src/scripts/helpers/variables.js
@@ -3,14 +3,32 @@ export const SIDEBAR = document.querySelector('#sidebar');
 export const PRELOADER = document.querySelector('.preload-wrapper');
 
 export const routesMap = new Map([
+  ['registration', { url: 'registration' }],
+  ['login', { url: 'login' }],
+  ['home', { url: '/'}],
   ['game1', { url: '#0', title: 'Учи слова' }],
   ['progress', { url: '#1', title: 'Твой прогресс' }],
   ['vocabulary', { url: '#2', title: 'Твой словарь' }],
-  ['speakit', { url: '#3', title: 'Произнеси слово' }],
-  ['englishpuzzle', { url: '#4', title: 'Английский паззл' }],
-  ['savannah', { url: '#5', title: 'Саванна' }],
-  ['audio', { url: '#6', title: 'Аудиовызов' }],
-  ['sprint', { url: '#7', title: 'Спринт' }],
+  [
+    'speakit',
+    { url: '#3', title: 'Произнеси слово', preview: require('../../assets/img/content/speakIt.png').default },
+  ],
+  [
+    'englishpuzzle',
+    { url: '#4', title: 'Английский пазл', preview: require('../../assets/img/content/speakIt.png').default },
+  ],
+  ['savannah', { url: '#5', title: 'Саванна', preview: require('../../assets/img/content/savannah.jpg').default }],
+  ['audio', { url: '#6', title: 'Аудиовызов', preview: require('../../assets/img/content/audiocall.jpg').default }],
+  ['sprint', { url: '#7', title: 'Спринт', preview: require('../../assets/img/content/sprint.png').default }],
+  ['ourGame', { url: '#', title: 'Наша игра', preview: require('../../assets/img/content/speakIt.png').default }],
   ['promo', { url: '#8', title: 'Промо' }],
   ['team', { url: '#9', title: 'Наша команда' }],
 ]);
+
+export const sidebarMap = ['game1', 'progress', 'vocabulary', 'speakit', 'englishpuzzle', 'savannah', 'audio', 'sprint', 'promo', 'team'].map((key) =>
+    routesMap.get(key)
+)
+
+export const gamesMap = ['speakit', 'englishpuzzle', 'savannah', 'audio', 'sprint', 'ourGame'].map((key) =>
+    routesMap.get(key)
+);
