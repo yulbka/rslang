b9765aa3dd21dc87cf4cc23c5690645a56c96d0e
marianna <vergunmari@gmail.com>
2020-06-26 01:42:31 +0300

    fix: router mounts before any data requests so pages which depend of store data render with initial values

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './css/index.scss';
-import { router } from './routes/index';
+import { initializeRouter, router } from './routes/index';
 import { store } from './store';
 import { API_USER } from './api/user';
-import { createSettingsBlock, createButtonToLearningWords, createBlockWithGames } from './pages/main';
-import { gamesMap, PRELOADER } from './scripts/helpers/variables';
-
-API_USER.getUser({ userId: localStorage.getItem('userId') })
-    .then((data) => {
-      if (data) {
-        router.navigate('/');
-      }
-    })
-    .finally(() => PRELOADER.classList.add('preload-wrapper-hidden'));
-
+import { routesMap, PRELOADER } from './scripts/helpers/variables';
 
 window.onload = async () => {
   await initRequests();
-  createButtonToLearningWords();
-  createSettingsBlock();
-  createBlockWithGames({ gamesData: gamesMap });
+  PRELOADER.classList.add('preload-wrapper-hidden')
+  initializeRouter()
 };
 
 async function initRequests() {
@@ -34,37 +23,3 @@ async function initRequests() {
     ...userSettings,
   };
 }
-
-//example
-(async () => {
-  const [user, userSettings] = await Promise.all([
-    API_USER.getUser({ userId: store.user.auth.userId, }),
-    API_USER.getUserSettings({
-      userId: store.user.auth.userId,
-    }),
-  ]);
-
-  Object.entries(user).forEach(([key, value]) => {
-    store.user.auth[key] = value;
-  });
-
-  Object.entries(userSettings).forEach(([key, value]) => {
-    store.user.learning[key] = value;
-  });
-
-  /*API_USER.setUserSettings({
-        userId: '5eea492edffad00017faa81c',
-        userSettings: {
-            "wordsPerDay": 551,  // must be less than or equal to 1000
-            "optional": {
-                "translation": true,
-                "withExplanation": false,
-                "withExample": false,
-                "transcription": true,
-                "image": false
-            }
-        }
-    });*/
-
-  console.log('usr', { ...store });
-})();
