5f27d5a5b94a4e1638a242bf73e9934c1f077d0b
Victoria Maximenko <victoriakakdela@mail.ru>
2020-07-15 13:31:09 +0300

    feat: add adaptive design for sprint

--- src/games/sprint/js/render.js
+++ src/games/sprint/js/render.js
@@ -13,17 +13,17 @@ export async function renderSprint() {
 
         <div class="row question">
 
-            <div class="answer-score col-3 d-flex text-center flex-position">
+            <div class="answer-score col-12 col-xl-3 col-lg-3 col-md-3 col-sm-3 d-flex text-center flex-position">
                 <span class="answer-score__title">Баллы за вопрос</span>
                 <span class="score-value" id="answerValue">10</span>
             </div>
 
-            <div class="question-card col-6 d-flex">
-                <div id="current-word" class="text-center">jetlag</div>
+            <div class="question-card col-12 col-xl-6 col-lg-6 col-md-6 col-sm-6 d-flex">
+                <div id="current-word" class="text-center"></div>
                 <div id="current-word-translation" class="text-center">должностная инструкция</div>
             </div>
 
-            <div class="total-score col-3 d-flex text-center flex-position">
+            <div class="total-score col-12 col-xl-3 col-lg-3 col-md-3 col-sm-3 d-flex text-center flex-position">
                 <span class="total-score__title">Всего баллов</span>
                 <span id="score" class="score-value">0</span>
             </div>
